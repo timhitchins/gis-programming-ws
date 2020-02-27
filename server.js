5fcaed0e1755d65452252c72804566af0928ee0a
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-02-27 09:37:05 -0800

    update initial slides

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.use(express.static(path.join(__dirname, 'gis_programming_workshop')));
+app.use(express.static(path.join(__dirname, "gis_programming_workshop")));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/gis_programming_workshop/index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/gis_programming_workshop/index.html"));
 });
 
-app.listen(8080);
+app.listen(8080, () => console.log("Listening on port 8080..."));
